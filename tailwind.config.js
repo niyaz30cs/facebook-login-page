5d2aa7fc9ca58ad94c3216a49012910a6590a915
Niyaz Alam <110074633+niyaz30cs@users.noreply.github.com>
2024-02-05 11:43:07 +0530

    done2

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["*"],
   theme: {
+    screens: {
+      xsm: '350px',
+      sm:'640px',
+      md:'768px',
+      lg:'1024px',
+      xl:'1280px',
+      xxl:'1536px'
+    },
     extend: {},
   },
   plugins: [],
